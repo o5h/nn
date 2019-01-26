49ffc264a8b974089e7c93bfb11e586b9f5c994d
Oleksandr Shaposhnikov <olexander.shaposhnikov@gmail.com>
2019-01-26 23:33:22 +0100

    some noise

--- examples/xor/main.go
+++ examples/xor/main.go
@@ -18,25 +18,29 @@ var data = []struct {
 }
 
 func main() {
-	seed := time.Now().UTC().UnixNano()
-	rand.Seed(seed)
-	n := nn.New(2, 3, 1)
+	for k := 0; k < 1000; k++ {
+		seed := time.Now().UTC().UnixNano()
+		rand.Seed(seed)
+		log.Printf("seed %d ", seed)
+		n := nn.New(3, 3, 1)
 
-	generation := 0
-	for n.Error > 0.001 {
+		generation := 0
+		for n.Error > 0.001 {
+			for _, d := range data {
+				n.FeedForward([]float64{d.A, d.B,
+					//some noise extremely speedups education
+					rand.Float64()})
+				n.BackPropagation([]float64{d.C})
+			}
+			generation++
+			//log.Printf("Generation %d : error= %f", generation, n.Error)
+		}
+
+		log.Printf("Generation %d : error= %f", generation, n.Error)
 		for _, d := range data {
 			n.FeedForward([]float64{d.A, d.B})
-			n.BackPropagation([]float64{d.C})
+			result := n.GetResults()[0]
+			log.Printf("%d XOR %d : %d = %d", int(d.A), int(d.B), int(d.C), int(result+0.5))
 		}
-		generation++
-
 	}
-
-	log.Printf("seed %d : Generation %d : error= %f", seed, generation, n.Error)
-	for _, d := range data {
-		n.FeedForward([]float64{d.A, d.B})
-		result := n.GetResults()[0]
-		log.Printf(" Expected %f : actual = %f", d.C, result)
-	}
-
 }
